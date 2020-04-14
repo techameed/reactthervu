c701151801c03476f177db79f5962dfb8a49f259
sairamya1703 <sairamya1703@gmail.com>
2020-04-14 19:08:58 +0530

    TR001: Route Config - Changed all let and var to const variable, removed console logs and place correct path in json file

--- src/app.routes.js
+++ src/app.routes.js
@@ -3,16 +3,15 @@ import permission from './path.json';
 import { Redirect, Route } from "react-router-dom"; 
 
 const ProtectedRoute = ({ component: Component, role, path, ...rest}) => {
-    let validatePath = (role, path) => {
-        var index = permission.admin.indexOf(path);
-        console.log(index);
-        if(index != -1) {
+    const validatePath = (path) => {
+        const index = permission.role.indexOf(path);
+        if(index !== -1) {
             return true;
         } else {
             return false;
         }
     }
-    let isAllowed = validatePath();
+    const isAllowed = validatePath();
     return (
         <Route path={path} {...rest} render={props => {
             return isAllowed ? <Component {...props}/> : <Redirect to="/" />;
