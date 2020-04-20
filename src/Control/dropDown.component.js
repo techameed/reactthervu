09686082e977d9271f225dada77ba208557bfc18
sairamya1703 <sairamya1703@gmail.com>
2020-04-20 22:23:46 +0530

    TR006: Drop Down component implementation

---
+++ src/Control/dropDown.component.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const DropDown = (props) => {
+    const {id, value, onChange, option} = props;
+            
+    return (
+        <select id={id} value={value} onChange={onChange}>        
+           {
+             option.length !== 0 ? option.map(year =>(<option value={year}>{year}</option>)) :
+             (<option>No data found</option>)        
+           }
+        </select>
+    );
+}
+
+export default DropDown;
