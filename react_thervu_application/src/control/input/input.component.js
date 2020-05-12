8ed21ac07eba40e4917a605c23d785612c24f338
Lavanya_ramesh <lavanyaramesh2903@gmail.com>
2020-05-12 20:11:34 +0530

    Code implementation for TR004_input component with corrected spacing

--- react_thervu_application/src/control/input/input.component.js
+++ react_thervu_application/src/control/input/input.component.js
@@ -3,27 +3,27 @@ import PropTypes from 'prop-types';
 import classes from '../input/input.css';
 
 const Input = (props) => {
-	return (
-		<div className={classes.inputControl}>
-			<input
-				id={props.id}
-				type={props.type}
-				value={props.value}
-				checked={props.checked}
-				onCheckbox={props.onCheckbox}
-				onChange={props.onChange}
-				placeholder={props.placeholder}/>
-		</div>
+  return (
+    <div className={classes.inputControl}>
+      <input
+        id={props.id}
+        type={props.type}
+        value={props.value}
+        checked={props.checked}
+        onCheckbox={props.onCheckbox}
+        onChange={props.onChange}
+        placeholder={props.placeholder}/>
+    </div>
   );
 }
 
 Input.propTypes = {
-	type: PropTypes.string,
-	checked: PropTypes.bool,
-	value: PropTypes.string,
-	onCheckbox: PropTypes.func,
-	onChange: PropTypes.func,
-	placeholder: PropTypes.string,
+  type: PropTypes.string,
+  checked: PropTypes.bool,
+  value: PropTypes.string,
+  onCheckbox: PropTypes.func,
+  onChange: PropTypes.func,
+  placeholder: PropTypes.string,
 };
 
 export default Input;
