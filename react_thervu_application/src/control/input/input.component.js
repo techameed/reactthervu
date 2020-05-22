d224295b9e92a57b735ccb75939655aadfcccf78
Lavanya_ramesh <lavanyaramesh2903@gmail.com>
2020-05-22 18:03:46 +0530

    Modified Implementation code for TR004_input component

--- react_thervu_application/src/control/input/input.component.js
+++ react_thervu_application/src/control/input/input.component.js
@@ -5,31 +5,33 @@ import classes from './input.css';
 const Input = props => {
 	
   const { 
-		  id, 
-		  type, 
-		  value, 
-		  checked, 
-		  placeholder, 
-		  onChange 
-		} = props;
-	
+          id, 
+  	      type, 
+  	      value, 
+  	      checked, 
+  	      disabled, 
+  	      placeholder, 
+  	      onChange 
+  	    } = props;
+  
   return (
     <div className={classes.inputControl}>
-	  <input
+      <input
         id={id}
         type={type}
+        disabled={disabled}
         value={value}
         checked={checked}
         onChange={onChange}
         placeholder={placeholder}/>
-	</div>
+    </div>
   );
 }
 
 Input.propTypes = {
   type: PropTypes.string,
-  value: PropTypes.string,
   checked: PropTypes.bool,
+  value: PropTypes.string,
   onChange: PropTypes.func,
   placeholder: PropTypes.string,
 };
