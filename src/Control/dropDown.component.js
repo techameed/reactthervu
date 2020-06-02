46e7643b3565e13a389b439d1c1111ee52f57c4e
sairamya1703 <sairamya1703@gmail.com>
2020-06-02 16:34:23 +0530

    TR004: Added a new prop (isPlaceholderHidden) to Drop Down component

--- src/Control/dropDown.component.js
+++ src/Control/dropDown.component.js
@@ -1,12 +1,21 @@
 import React from 'react';
 
 const DropDown = (props) => {
-  const {id, value, onChange, option} = props;
+  const {id, value, onChange, option, isPlaceholderHidden} = props;
 
   return (
     <select id={id} value={value} onChange={onChange}>        
       {option.length !== 0 
-        ? option.map(data =>(<option value={data}>{data}</option>)) 
+        ? option.map((data, index) => 
+          (
+          <option 
+            key={index} 
+            hidden={isPlaceholderHidden && index === 0} 
+            value={data}>
+          {data}
+          </option>
+          )
+        ) 
         : (<option>No data found</option>)        
       }
     </select>
